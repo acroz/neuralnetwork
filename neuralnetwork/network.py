b1b4e7cd4442340f8b2ff63fe2b3ff1b1c7d81a0
Andrew Crozier <wacrozier@gmail.com>
2016-06-21 01:00:25 +0200

    Moved CategoryMapper to its own module

--- neuralnetwork/network.py
+++ neuralnetwork/network.py
@@ -1,8 +1,8 @@
 
 import numpy as np
 from scipy.optimize import minimize
-from neuralnetwork.layer import NeuralNetworkLayer
-
+from .layer import NeuralNetworkLayer
+from .categorymapper import CategoryMapper
 
 def uniform_initializer(shape, epsilon=0.12):
     """
@@ -16,67 +16,6 @@ def gaussian_initializer(shape, sigma=1.0):
     Initialise array with a Gaussian distribution centred on zero.
     """
     return np.random.randn(*shape) * sigma
-
-
-class CategoryMapper(object):
-    """
-    Map between categories and output activations.
-
-    Parameters
-    ----------
-    size : int
-        The number of categories to map
-    """
-
-    def __init__(self, size):
-        self._size = int(size)
-        self._categories = None
-
-    def register(self, labels):
-        """
-        Register some categories for a set of labels.
-
-        Parameters
-        ----------
-        labels : array_like
-            A set of labels to register categories for.
-        """
-        self._categories = np.unique(labels)
-        assert len(self._categories) == self._size
-
-    def activations(self, labels):
-        """
-        Map a set of labels to their corresponding activations.
-
-        Parameters
-        ----------
-        labels : array_like
-            The labels to map
-
-        Returns
-        -------
-        activations : array
-            The corresponding activations
-        """
-        assert self._categories is not None
-        return labels == self._categories[:,np.newaxis]
-
-    def labels(self, activations):
-        """
-        Map a set of activations to their corresponding labels.
-
-        Parameters
-        ----------
-        activations : array_like
-            The activations to map
-
-        Returns
-        -------
-        labels : array
-            The corresponding labels
-        """
-        assert self._categories is not None
-        return self._categories[activations.argmax(axis=0)]
 
 
 class NeuralNetwork(object):
